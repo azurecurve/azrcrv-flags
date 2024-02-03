395f2985e7759aadac51bd3f85085767aa7f08fc
azurecurve <github@azurecurve.co.uk>
2024-02-03 14:06:46 +0000

    Release v2.1.4

--- azrcrv-flags.php
+++ azrcrv-flags.php
@@ -1,14 +1,18 @@
 <?php
 /**
  * ------------------------------------------------------------------------------
- * Plugin Name: Flags
- * Description: Allows flags to be added to posts and pages using a shortcode.
- * Version: 2.1.3
- * Author: azurecurve
- * Author URI: https://development.azurecurve.co.uk/classicpress-plugins/
- * Plugin URI: https://development.azurecurve.co.uk/classicpress-plugins/flags/
- * Text Domain: azrcrv-f
- * Domain Path: /languages
+ * Plugin Name:		Flags
+ * Description:		Allows flags to be added to posts and pages using a shortcode.
+ * Version:			2.1.4
+ * Requires CP:		1.0
+ * Author:			azurecurve
+ * Author URI:		https://development.azurecurve.co.uk/classicpress-plugins/
+ * Plugin URI:		https://development.azurecurve.co.uk/classicpress-plugins/flags/
+ * Donate link:		https://development.azurecurve.co.uk/support-development/
+ * Text Domain:		azrcrv-f
+ * Domain Path:		/assets/languages
+ * License:			GPLv2 or later
+ * License URI:		http://www.gnu.org/licenses/gpl-2.0.html
  * ------------------------------------------------------------------------------
  * This is free software released under the terms of the General Public License,
  * version 2, or later. It is distributed WITHOUT ANY WARRANTY; without even the
